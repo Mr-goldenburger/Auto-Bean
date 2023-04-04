93bc70c60646e0b0b28c704135b6c62f4ac7a6dd
Michael Hernandez <mikehern2203@gmail.com>
2023-04-04 18:40:54 -0500

    Updated to v4.5 - see changes in UpdateLog
    Time it takes to answer questions is now based on the allotted time for the question. Skipping questions is also based on this, so hopefully no more getting stuck on the restudy screen.

---
+++ v4.5.js
@@ -0,0 +1,172 @@
+var interval = {
+	"test": "",
+	"click": "",
+	"deltest": "",
+} 
+var bool = {
+	"bot": true,
+	"on": true,
+	"skip": true,
+} 
+var object = {
+	"div": "",
+	"button1": "",
+}
+
+object.div = document.createElement("DIV");
+object.div.style.width = "202px";
+object.div.style.height = "26px";
+object.div.style.border = "3px solid black";
+object.div.id = "div";
+object.div.style.background = "gray";
+
+object.button2 = document.createElement("BUTTON");
+object.button2.style.width = "100px";
+object.button2.style.height = "25px";
+object.button2.style.border = "3px solid black";
+object.button2.style.background = "white";
+object.button2.id = "button2";
+object.button2.innerHTML = "Skip: On";
+object.button2.setAttribute("onclick", "skip()");
+
+object.button1 = document.createElement("BUTTON");
+object.button1.style.width = "100px";
+object.button1.style.height = "25px";
+object.button1.style.border = "3px solid black";
+object.button1.style.background = "white";
+object.button1.id = "button1";
+object.button1.innerHTML = "On";
+object.button1.setAttribute("onclick", "start()");
+
+document.body.appendChild(object.div);
+document.getElementById("div").appendChild(object.button1);
+document.getElementById("div").appendChild(object.button2);
+
+var no_content = 0
+
+var accu_reduction = 0
+var targ_accu = 0
+var disp_accu = 0
+while (targ_accu == 0){
+	accu_reduction = (Math.random() * 15);
+	targ_accu = 93 - accu_reduction;
+	disp_accu = targ_accu.toFixed(0);
+	targ_accu = targ_accu / 100;
+	console.log("Session Accuracy Target is " + disp_accu +"%");
+}
+
+
+function test() {
+	if(bool.bot == true) {
+		if(document.Pass) {
+			bool.bot = false;
+			set("click");
+		} 
+		else if (document.getElementById("next-btn")) {
+			set("stop");
+			let x = document.querySelectorAll("#choice-section li");
+			console.log("Next button clicked");
+			x[0].click();
+			x[1].click();
+			x[2].click();
+			document.getElementById("next-btn").click();
+			set("test");
+		} 
+		else if (no_content >= 1000) {
+			console.log("No question detected. Resetting system...")
+			set("stop");
+			let x = document.querySelectorAll("#choice-section li");
+			console.log("Next button clicked");
+			x[0].click();
+			x[1].click();
+			x[2].click();
+			document.getElementById("next-btn").click();
+			set("test");
+		} 
+		else {
+			console.log("Scanning for question");
+			no_content = no_content + 1				
+		}
+	}
+} 
+
+
+function set(type) {
+	if(type == "click") { //waits time to click
+		no_content = 0
+		if(Math.random() > targ_accu && bool.skip == true) {
+			let time = (alotted_time() + 10) * 1000;
+			console.log("Skipping question");
+			setTimeout(() => {set("test")}, time);
+		}
+		else if (alotted_time()) {
+			let time = alotted_time() - 2;
+			console.log("This question will be answered in", time, "seconds");
+			time = time * 1000;
+			interval.click = setTimeout(press, time);
+		}
+		else {
+			console.log("This question will be answered in", 30, "seconds");
+			interval.click = setTimeout(press, 30000);
+		}
+	} 
+	else if (type == "test") {//starts testing after 2 seconds
+		interval.deltest = setTimeout(function x(){bool.bot = true;}, 2000);
+	} 
+	else if (type == "stop") { //CLEAR ALL TIMEOUTS AND INTERVALS
+		clearTimeout(interval.click);
+		clearTimeout(interval.deltest);
+		bool.bot = false; 
+	}
+
+
+	function alotted_time(){
+		try{
+			const timerContainer = document.getElementById('timer-container');
+			const timeoutValue = timerContainer.getAttribute('data-timeout');
+			return timeoutValue
+		}
+		catch {TypeError}
+	}
+} 
+
+
+function press() {
+	document.Pass.click();
+	console.log("Question answered");
+	set("test");
+	
+} 
+
+
+function start() {
+	if(bool.on == true) {
+		set("stop");
+		bool.on = false;
+		bool.bot = false;
+		set("stop");
+		console.log("Bot Inactive");
+		document.getElementById("button1").innerHTML = "Off";
+	} else if (bool.on == false) {
+		bool.on = true;
+		bool.bot = true;
+		console.log("Bot Active");
+		document.getElementById("button1").innerHTML = "On";
+	}
+
+} 
+
+function skip() {
+	if(bool.skip == true) {
+		bool.skip = false;
+		console.log("Answering all questions");
+		document.getElementById("button2").innerHTML = "Skip: Off";
+	
+	} else if (bool.skip == false) {
+		bool.skip = true;
+		console.log("Skipping some questions");
+		document.getElementById("button2").innerHTML = "Skip: On";
+	}
+}
+
+interval.test = setInterval(test, 1);
